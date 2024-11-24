0287785dcdd9417543e5a0cefdbe2cde32e03d05
hajaemin <woals3527@naver.com>
2024-11-24 23:20:59 +0900

    배포완료

--- src/main/java/org/sunbong/chatbot_api/ChatbotApiApplication.java
+++ src/main/java/org/sunbong/chatbot_api/ChatbotApiApplication.java
@@ -1,6 +1,5 @@
 package org.sunbong.chatbot_api;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -10,12 +9,6 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 public class ChatbotApiApplication {
 
 	public static void main(String[] args) {
-		// .env 파일 읽기
-		Dotenv dotenv = Dotenv.configure().load();
-
-		// .env 값을 시스템 속성으로 설정
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
 		SpringApplication.run(ChatbotApiApplication.class, args);
 	}
 }
